02bb1e8691e149f9db6ccd3253b422ce7a84c154
rotateX <homshin@outlook.com>
2018-08-31 17:26:46 +0800

    登录密码验证问题

--- apps/login/signupform.py
+++ apps/login/signupform.py
@@ -1,32 +1,62 @@
 from django import forms
 
 class signupform(forms.Form):
-    username = forms.CharField(min_length=5, max_length=16, widget=forms.TextInput(attrs={
-        'type': 'text',
-        'class': 'text',
-        'value': 'User Name',
-        'onfocus': "this.value = '';",
-        'onblur': "if (this.value == '') {this.value = 'User Name';}",
-        'id':"active"
-    }))
-    email = forms.EmailField(min_length=5, max_length=254, widget=forms.EmailInput(attrs={
-        'type': 'text',
-        'class': 'text',
-        'value': '你的邮箱',
-        'onfocus': "this.value = '';",
-        'onblur': "if (this.value == '') {this.value = 'your@email.com';}",
+    username = forms.CharField(min_length=5,
+                               max_length=16,
+                               widget=forms.TextInput(attrs={
+                                   'type': 'text',
+                                   'class': 'text',
+                                   'value': 'User Name',
+                                   'onfocus': "this.value = '';",
+                                   'onblur': "if (this.value == '') {this.value = 'User Name';}",
+                                   'id':"active"
+                                                            }
+                               ),
+                               error_messages={
+                                   'required': '用户名不能为空',
+                                   'min_length': '太短了',
+                                   'max_length': '太长了',
+                                                }
+                               )
+    email = forms.EmailField(min_length=5,
+                             max_length=254,
+                             widget=forms.EmailInput(attrs={
+                                 'type': 'text',
+                                 'class': 'text',
+                                 'value': '你的邮箱',
+                                 'onfocus': "this.value = '';",
+                                 'onblur': "if (this.value == '') {this.value = 'your@email.com';}",
     }))
     password = forms.CharField(min_length=5,max_length=32,widget=forms.PasswordInput(attrs={
-        'type': 'text',
+        'type': 'password',
         'class': 'text',
         'value': '输入密码',
         'onfocus': "this.value = '';",
         'onblur': "if (this.value == '') {this.value = 'Password';}",
     }))
     password2 = forms.CharField(min_length=5,max_length=32,widget=forms.PasswordInput(attrs={
-        'type': 'text',
+        'type': 'password',
         'class': 'text',
         'value': '确认密码',
         'onfocus': "this.value = '';",
         'onblur': "if (this.value == '') {this.value = 'Password';}",
     }))
+
+class loginform(forms.Form):
+    username = forms.CharField(min_length=5,
+                               max_length=16,
+                               widget=forms.TextInput(attrs={
+                                   'type': 'text',
+                                   'class': 'form-username form-control',
+                                   'value': 'User Name',
+                                   'id':"form-username",
+                                   'placeholder': "Username...",
+                                   'name':'form-username',
+                                                            }
+                               ),
+                               error_messages={
+                                   'required': '用户名不能为空',
+                                   'min_length': '太短了',
+                                   'max_length': '太长了',
+                                                }
+                               )
--- apps/login/views.py
+++ apps/login/views.py
@@ -3,28 +3,25 @@ from apps.login.models import *
 from django.contrib.auth.hashers import make_password, check_password
 from django.db.models import Q
 from apps.login.signupform import signupform
-
+from django.contrib import auth
 # Create your views here.
 # 用户登录
 def signin(request):
     if request.method == 'POST':
         username = request.POST.get('form-username', None) # 确保当数据请求中没有username键时不会抛出异常，而是返回默认值None；
-        password = request.POST.get('form-password', None)
-        email = request.POST.get('form-username', None)
-        print('get'+password)
-        print(UserInfo.objects.filter(username=username).values('password'))
-        # if UserInfo.objects.filter(Q(username=username) | Q(email=email)):
+        password1 = request.POST.get('form-password', None)
+        if UserInfo.objects.filter(Q(username=username) | Q(email=username)):
         # if UserInfo.objects.filter(username=username):
-
-        if password == UserInfo.objects.filter(username=username).values('password'):
-
-            request.session['username'] = username
-            request.session['is_login'] = True
-            return redirect('home')
-        else:
-            return render(request, 'signin/signin.html', {
-                'msg': '用户名或密码错误'
-            })
+            a = UserInfo.objects.get(username=username)
+            b = a.password
+            if check_password(password1, b):
+                request.session['username'] = username
+                request.session['is_login'] = True
+                return redirect('home')
+            else:
+                return render(request, 'signin/signin.html', {
+                    'msg': '用户名或密码错误'
+                })
     else:
         return render(request, 'signin/signin.html')
 
@@ -47,12 +44,12 @@ def index(request):
 
 # 注册用户
 def signup(request):
-    # if request.method == 'POST':
-    #     username = request.POST.get('username')
-    #     password = request.POST.get('password')
-    #     email = request.POST('email')
-    #     UserInfo.objects.create(username=username.strip(), password=make_password(password), email=email.strip(), is_active=False)
-    sform = signupform(request.POST)
-    return render(request, 'signup/register.html', {
-        'form': sform
-    })
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        confirm_p = request.POST.get('confim_p')
+        email = request.POST.get('email')
+        if password == confirm_p and UserInfo.objects.filter(username=username).count()<=1:
+            UserInfo.objects.create(username=username.strip(), password=make_password(password, None,'default'), email=email.strip(), is_active=False)
+            print(check_password(password, UserInfo.objects.filter(username=username).values('password')))
+    return render(request, 'signup/register.html')
